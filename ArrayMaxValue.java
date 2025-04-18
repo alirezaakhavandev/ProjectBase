4f9da00e3d9acf26d616ca5bb75fb53ac25bea7f
Alireza <alireza.akhavan.work@gmail.com>
2025-04-18 04:57:55 -0400

    Update ArrayMaxValue.java

--- ArrayMaxValue.java
+++ ArrayMaxValue.java
@@ -5,8 +5,13 @@ import java.util.Arrays;
 
 public class App {
     public static void main( String[] args ) {
-        int[] numbers = {5,6,28,32,78,1,2,5,6,7,-12};
-        System.out.println(Arrays.toString(getEven(numbers)));
+        int[] numbers = {5,6,28,32,78,1,2,5,6,7,-12,33,67,129,321,-451};
+        System.out.println("The max value is : " + maxValue(numbers));
+        System.out.println("The min value is : " + minValue(numbers));
+        System.out.println("The index of Max value is : " + maxIndex(numbers));
+        System.out.println("The reverse order is : " + Arrays.toString(reverseOrder(numbers)));
+        System.out.println("Even numbers are : " + Arrays.toString(getEven(numbers)));
+        System.out.println("Odd numbers are : "  + Arrays.toString(getOdd(numbers)));
 
     }
 
@@ -28,7 +33,7 @@ public class App {
         }
         return min;
     }
-    static int findTheIndex(int[] arr){
+    static int maxIndex(int[] arr){
         int i = 0;
         int max = maxValue(arr);
         while (arr[i] != max){
@@ -43,7 +48,6 @@ public class App {
         }
         return reverseTheNumbers;
     }
-
     static int[] getEven(int[] array){
         int count = 0;
         //gets the amount of even numbers in an array
@@ -56,7 +60,7 @@ public class App {
         int counter = 0;
 
         int[] values = new int[count];
-        
+
         for (int i : array){
             if (i % 2 == 0){
                 values[counter] = i;
@@ -64,7 +68,32 @@ public class App {
             }
         }
         return values;
-        
+
     }
+
+
+    static int[] getOdd(int[] array){
+        // count the even numbers to get the length
+        int count = 0 ;
+        for (int i : array){
+            if (i % 2 != 0){
+                count++;
+            }
+        }
+        // create a new array using that length
+        int [] oddArray = new int[count];
+        // create a counter for the index of the new array to be updated each time it finds a new value for it
+        int indexCounter = 0;
+        // add values to it
+        for (int i : array){
+            if (i % 2 != 0){
+                oddArray[indexCounter] = i;
+                indexCounter++;
+            }
+        }
+        //return array
+        return oddArray;
+    }
+
 
 }
